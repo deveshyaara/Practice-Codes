fb9f208fbe46951cc68db33d831c48d520cceded
deveshyaara <161412073+deveshyaara@users.noreply.github.com>
2024-04-07 21:36:11 +0530

    Add files via upload

---
+++ loop4.cpp
@@ -0,0 +1,23 @@
+//Find the sum of the following series
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int result=0;
+    for(int i=1;i<=n;i++)
+    {
+        if (i%2==0)
+        {
+            result-=i;
+        }
+        else
+        {
+            result+=i;
+        }
+       
+    }
+     cout<<result<<endl; 
+    return 0;
+}
