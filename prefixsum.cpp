ac623df141df96f90d7f4a128d9a86dc0b529dd9
deveshyaara <161412073+deveshyaara@users.noreply.github.com>
2024-04-08 11:05:46 +0530

    Update prefixsum.cpp

--- prefixsum.cpp
+++ prefixsum.cpp
@@ -1,5 +1,5 @@
-\*Given an integer array ‘a’, return the prefix sum/ running sum in the same array without creating a 
-new array.*\
+/*Given an integer array ‘a’, return the prefix sum/ running sum in the same array without creating a 
+new array.*/
 #include<iostream>
 #include<vector>
 using namespace std;
