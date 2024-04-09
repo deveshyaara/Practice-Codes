95ff53f9afd6f5b09eec8b6576da8f51c81972fc
deveshyaara <tiwaridevesh887@gmail.com>
2024-04-09 13:06:51 +0530

    new file:   matrixMultiplication.cpp

---
+++ matrixMultiplication.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r1, c1;
+    cin>>r1>>c1;
+    int A[r1][c1];
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            cin>>A[i][j];
+        }  
+    }
+     int r2, c2;
+    cin>>r2>>c2;
+    int B[r1][c1];
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            cin>>B[i][j];
+        }
+    }
+    if (c1!=r2)
+    {       
+        cout<<"Matrix multiplication is not possible";
+    }
+    else
+    {    
+    int C[r1][c2];
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            int value =0;
+            for (int k = 0; k < r2; k++)
+            {
+                value+=A[i][k]*B[k][j];
+            }C[i][j]=value;
+            
+        }   
+    }
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            cout<<C[i][j]<<" ";
+        }cout<<endl;
+    }
+    }
+    return 0;
+}
