389a658102546d15897fe1317c32f85dab960579
deveshyaara <tiwaridevesh887@gmail.com>
2024-04-21 01:03:36 +0530

    modified:   2Darray4.cpp

--- 2Darray4.cpp
+++ 2Darray4.cpp
@@ -3,6 +3,33 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int leftMostOneRow(vector<vector<int>> &V)
+{
+    int leftMostOne = -1;
+    int maxOnesRow = -1;
+    int j = V[0].size()-1;
+    // Finding the left most one in 0th row
+    while (j>=0 && j==1)
+    {
+        leftMostOne= j;
+        maxOnesRow = 0;
+        j--;
+    }
+    // Check in rest of the row if we can find a one left to the leftMostOne
+    for (int i = 0; i < V.size(); i++)
+    {
+        while (j>=0 && V[i][j]==1)
+        {
+            leftMostOne=j;
+            j--;
+            maxOnesRow=i;
+        }
+        
+    }
+    return maxOnesRow;
+    
+    
+}
 int maximumOnesRow(vector<vector<int>> &V)
 {
     int maxOnes=INT16_MIN;
