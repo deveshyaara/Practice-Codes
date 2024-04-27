5922340e2d26fd6c2581dcf31590c026a56f3074
deveshyaara <tiwaridevesh887@gmail.com>
2024-04-27 19:47:08 +0530

    new file:   prefixSumIn2DArray1.2.cpp

--- prefixSumIn2DArray1.2.cpp
+++ prefixSumIn2DArray1.2.cpp
@@ -3,16 +3,28 @@ Return the sum of the rectangle from (l1,r1) to (l2,r2)*/
 #include<iostream>
 #include<vector>
 using namespace std;
-int Solve(vector<vector<int>> matrix, int l1, int r1, int l2, int r2)
+int rectangleSum(vector<vector<int>> matrix, int l1, int r1, int l2, int r2)
 {
     int sum=0;
-    for (int i = l1; i <=l2; i++)
+    // Prefix sum row-wise
+    for (int i = 0; i < matrix.size(); i++)
     {
-        for (int j = r1; j <= r2; j++)
+        for (int j = 1; j < matrix[0].size(); j++)
         {
-            sum+=matrix[i][j];
+            matrix[i][j]+=matrix[i][j-1];
         }
-        
+    }
+    for (int i = l1; i <= l2; i++)
+    {
+        if (r1!=0)
+        {
+           sum+= matrix[i][r2]-matrix[i][r1-1]; 
+        }
+        else
+        {
+            // matrix[i][r1-1]=0
+            sum+=matrix[i][r2];
+        }
     }
     return sum;
 }
@@ -38,7 +50,6 @@ int main()
             cout<<matrix[i][j]<<" ";
         }cout<<endl;
     }
-    
-    cout<<"Sum is:"<<Solve(matrix, l1, l2, r1, r2)<<endl;
+    cout<<"Sum = "<<rectangleSum(matrix, l1, l2, r1, r2)<<endl;
     return 0;
 }
